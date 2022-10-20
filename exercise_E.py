2e8fbd24fd15e7b6142d0a2b60380bbfc8bdfad5
toshi/pono000 <66683209+toshi-pono@users.noreply.github.com>
2022-10-20 18:21:26 +0900

    🎉 pipelineとGridSearchを使った

--- exercise_E.py
+++ exercise_E.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LassoCV
+from sklearn.linear_model import Lasso
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error
 
 import loadData
@@ -11,31 +13,42 @@ print("loading...")
 train_df, ans_df = loadData.load_data_from_smile_csv(
     "data/SM.csv", use_cache=True)
 
-print("fitting...")
-scaler = StandardScaler()
-clf = LassoCV(alphas=10 ** np.arange(-6, 1, 0.1), cv=5)
 
-scaler.fit(train_df)
-clf.fit(scaler.transform(train_df), ans_df)
-# LassoCV(alphas=array([1.00000e-06, 1.2
-# 5893e-06, ..., 6.30957e+00, 7.94328e+00]),
-# copy_X=True, cv=5, eps=0.001, fit_intercept=True, max_iter=1000,
-# n_alphas=100, n_jobs=None, normalize=False, positive=False,
-# precompute='auto', random_state=None, selection='cyclic', tol=0.0001,
-# verbose=False)
+print("fitting...")
+pipeline = Pipeline(steps=[('scaler', StandardScaler()), ('model', Lasso())])
+glf = GridSearchCV(pipeline, param_grid={
+                   "model__alpha": np.arange(0.001, 0.01, 0.0001)}, cv=5, scoring="neg_root_mean_squared_error")
+glf.fit(train_df, ans_df)
+
 print("done")
 
-# 最良パラメータ
-print(f"Best Alpha (from MSE path):{clf.alpha_}")
+print(-glf.best_score_)
+print(glf.best_params_)
 
-# print(clf.coef_)
-# print(clf.intercept_)
+result_df = pd.DataFrame(glf.cv_results_)
+result_df.sort_values(
+    by="rank_test_score", inplace=True)
+print(result_df[["rank_test_score",
+                 "params",
+                 "mean_test_score"]])
 
-# 最良パラメータにおける InKa のRMSE
-# TODO: 
-print(f"MSE at Best Alpha:{clf.mse_path_[np.argmin(clf.mse_path_.mean(axis=-1),)]}")
-# print(clf.mse_path_)
-y_pred = clf.predict(scaler.transform(train_df))
-mse = mean_squared_error(ans_df, y_pred)
+# calc
+best = glf.best_estimator_
+y_pred = best.predict(train_df)
+print(np.sqrt(mean_squared_error(ans_df, y_pred)))
 
-print(f"{mse=}")
+# # 最良パラメータ
+# print(f"Best Alpha (from MSE path):{clf.alpha_}")
+
+# # print(clf.coef_)
+# # print(clf.intercept_)
+
+# # 最良パラメータにおける InKa のRMSE
+# # TODO:
+# print(
+#     f"MSE at Best Alpha:{clf.mse_path_[np.argmin(clf.mse_path_.mean(axis=-1),)]}")
+# # print(clf.mse_path_)
+# y_pred = clf.predict(scaler.transform(train_df))
+# mse = mean_squared_error(ans_df, y_pred)
+
+# print(f"{mse=}")
