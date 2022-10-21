fda3133b5fbbaeb3674473bb347ee4d405ed5578
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-10-21 14:41:31 +0900

    exF

---
+++ exercise_F.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Lasso
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error
+
+import loadData
+
+def main():
+    print("loading...")
+    train_df, ans_df = loadData.load_data_from_smile_csv(
+        "data/SM.csv", use_cache=True)
+    test_df, test_ans_df = loadData.load_data_from_smile_csv(
+        "data/CP.csv", use_cache=True)
+
+    print("fitting...")
+    pipeline = Pipeline(steps=[('scaler', StandardScaler()), ('model', Lasso())])
+    glf = GridSearchCV(pipeline, param_grid={
+                    "model__alpha": np.arange(0.001, 0.01, 0.0001)}, cv=5, scoring="neg_root_mean_squared_error")
+    glf.fit(train_df, ans_df)
+    print("done")
+    
+    print("----------------------------------------")
+    test_pred = glf.best_estimator_.predict(test_df)
+    print(f"テストデータ全体に対するRMSE: {np.sqrt(mean_squared_error(test_ans_df, test_pred))}")
+    print(f"相関係数: {np.corrcoef(test_ans_df, test_pred)[0, 1]}")
+    print("----------------------------------------")
+
+if __name__ == "__main__":
+    main()
--- loadData.py
+++ loadData.py
@@ -6,7 +6,7 @@ import exercise_D
 def load_data_from_smile_csv(file: str, use_cache=False) -> tuple[pd.DataFrame, pd.DataFrame]:
     """
     Load data from a csv file with the following columns:
-    SMILES,PPB (fb)
+    SMILES,PPB(fb)
     """
     if use_cache:
         try:
@@ -19,9 +19,30 @@ def load_data_from_smile_csv(file: str, use_cache=False) -> tuple[pd.DataFrame, 
     smile_df = pd.read_csv(file)
     descriptors_df = smile_df["SMILES"].map(exercise_C.smile_to_descriptor_vec)
     train_df = pd.DataFrame([row for row in descriptors_df])
-    ans_df = smile_df["PPB (fb)"].map(exercise_D.calc_lnka)
+    ans_df = smile_df["PPB(fb)"].map(parse_PPB).map(exercise_D.calc_lnka)
 
     # save cache
     train_df.to_pickle(file + ".train.pkl")
     ans_df.to_pickle(file + ".ans.pkl")
     return (train_df, ans_df)
+
+def parse_PPB(fb: str | float) -> float:
+    """
+    Parse the PPB(fb) column
+    """
+    if isinstance(fb, float):
+        return fb
+    if isinstance(fb, str):
+        if is_float(fb):
+            return float(fb)
+        else:
+            s = fb.split("-")
+            return (float(s[0]) + float(s[1])) / 2
+
+def is_float(s: str) -> bool:
+    try:
+        float(s)
+    except ValueError:
+        return False
+    else:
+        return True
