84295c84f012d0f2219e0c78500e9cdfb0b9688f
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-10-21 14:03:00 +0900

    出力を整理した

--- exercise_E.py
+++ exercise_E.py
@@ -9,46 +9,36 @@ from sklearn.metrics import mean_squared_error
 
 import loadData
 
-print("loading...")
-train_df, ans_df = loadData.load_data_from_smile_csv(
-    "data/SM.csv", use_cache=True)
+def fit_model():
+    print("loading...")
+    train_df, ans_df = loadData.load_data_from_smile_csv(
+        "data/SM.csv", use_cache=True)
 
+    print("fitting...")
+    pipeline = Pipeline(steps=[('scaler', StandardScaler()), ('model', Lasso())])
+    glf = GridSearchCV(pipeline, param_grid={
+                    "model__alpha": np.arange(0.001, 0.01, 0.0001)}, cv=5, scoring="neg_root_mean_squared_error")
+    glf.fit(train_df, ans_df)
+    print("done")
 
-print("fitting...")
-pipeline = Pipeline(steps=[('scaler', StandardScaler()), ('model', Lasso())])
-glf = GridSearchCV(pipeline, param_grid={
-                   "model__alpha": np.arange(0.001, 0.01, 0.0001)}, cv=5, scoring="neg_root_mean_squared_error")
-glf.fit(train_df, ans_df)
+    print(f"RMSE: {-glf.best_score_}")
+    print(f"最良パラメータ: {glf.best_params_}")
 
-print("done")
+    result_df = pd.DataFrame(glf.cv_results_)
+    result_df.sort_values(
+        by="rank_test_score", inplace=True)
+    result_df.to_csv("./output/result.csv")
+    print(result_df[["rank_test_score",
+                    "params",
+                    "mean_test_score"]])
 
-print(-glf.best_score_)
-print(glf.best_params_)
+    # 訓練データに対する予測を比較する
+    best = glf.best_estimator_
+    y_pred = best.predict(train_df)
+    print(f"訓練データ全体に対するRMSE: {np.sqrt(mean_squared_error(ans_df, y_pred))}")
 
-result_df = pd.DataFrame(glf.cv_results_)
-result_df.sort_values(
-    by="rank_test_score", inplace=True)
-print(result_df[["rank_test_score",
-                 "params",
-                 "mean_test_score"]])
+    # 相関係数
+    print(f"相関係数: {np.corrcoef(ans_df, y_pred)[0, 1]}")
 
-# calc
-best = glf.best_estimator_
-y_pred = best.predict(train_df)
-print(np.sqrt(mean_squared_error(ans_df, y_pred)))
-
-# # 最良パラメータ
-# print(f"Best Alpha (from MSE path):{clf.alpha_}")
-
-# # print(clf.coef_)
-# # print(clf.intercept_)
-
-# # 最良パラメータにおける InKa のRMSE
-# # TODO:
-# print(
-#     f"MSE at Best Alpha:{clf.mse_path_[np.argmin(clf.mse_path_.mean(axis=-1),)]}")
-# # print(clf.mse_path_)
-# y_pred = clf.predict(scaler.transform(train_df))
-# mse = mean_squared_error(ans_df, y_pred)
-
-# print(f"{mse=}")
+if __name__ == "__main__":
+    fit_model()
