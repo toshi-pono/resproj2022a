1180ffde675acddb5fcbefae3e9cc60638dfbbc9
toshi/pono000 <66683209+toshi-pono@users.noreply.github.com>
2022-10-24 17:14:10 +0900

    ♻️ 関数に切った

--- exercise_A.py
+++ exercise_A.py
@@ -1,5 +1,11 @@
 from rdkit import Chem
 from rdkit.Chem import Draw
 
-m = Chem.MolFromSmiles('CC(C)Cc1ccc(cc1)[C@@H](C)C(=O)O')
-Draw.MolToFile(m, 'output/exercise_A.png')
+
+def smiles_to_image(smiles: str, file: str) -> None:
+    mol_obj = Chem.MolFromSmiles(smiles)
+    Draw.MolToFile(mol_obj, file)
+
+
+if __name__ == "__main__":
+    smiles_to_image('CC(C)Cc1ccc(cc1)[C@@H](C)C(=O)O', 'output/exercise_A.png')
