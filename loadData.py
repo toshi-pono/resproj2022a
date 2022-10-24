db5ce98a0907994d2242c8f752a05f95e4b098ee
toshi/pono000 <66683209+toshi-pono@users.noreply.github.com>
2022-10-24 17:01:43 +0900

    👕 コメントの整備

--- loadData.py
+++ loadData.py
@@ -7,24 +7,36 @@ def load_data_from_smile_csv(file: str, use_cache=False) -> tuple[pd.DataFrame, 
     """
     Load data from a csv file with the following columns:
     SMILES,PPB(fb)
+
+    Parameters
+    ----------
+    file : str
+        The path to the csv file
+    use_cache : bool, optional
+        If True, use cache, by default False
+
+    Returns
+    -------
+    data: tuple[pd.DataFrame, pd.DataFrame]
+        The first element is the input data, the second element is the answer data
     """
     if use_cache:
         try:
-            train_df = pd.read_pickle(file + ".train.pkl")
-            ans_df = pd.read_pickle(file + ".ans.pkl")
-            return (train_df, ans_df)
+            input_df = pd.read_pickle(file + ".input.pkl")
+            answer_df = pd.read_pickle(file + ".ans.pkl")
+            return (input_df, answer_df)
         except FileNotFoundError:
             pass
 
     smile_df = pd.read_csv(file)
     descriptors_df = smile_df["SMILES"].map(exercise_C.smiles_to_descriptors)
-    train_df = pd.DataFrame([row for row in descriptors_df])
-    ans_df = smile_df["PPB(fb)"].map(parse_PPB).map(exercise_D.calc_lnka)
+    input_df = pd.DataFrame([row for row in descriptors_df])
+    answer_df = smile_df["PPB(fb)"].map(parse_PPB).map(exercise_D.calc_lnka)
 
     # save cache
-    train_df.to_pickle(file + ".train.pkl")
-    ans_df.to_pickle(file + ".ans.pkl")
-    return (train_df, ans_df)
+    input_df.to_pickle(file + ".input.pkl")
+    answer_df.to_pickle(file + ".ans.pkl")
+    return (input_df, answer_df)
 
 
 def parse_PPB(fb: str | float) -> float:
@@ -42,6 +54,9 @@ def parse_PPB(fb: str | float) -> float:
 
 
 def is_float(s: str) -> bool:
+    """
+    Check if a string is a float
+    """
     try:
         float(s)
     except ValueError:
