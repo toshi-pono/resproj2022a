24349125c30ecfbc2b80f9ba02d9410d62a3d885
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-10-26 01:14:45 +0900

    🐛 不要なファイルを削除

--- exercise_I2.py
+++ exercise_I2.py
@@ -6,6 +6,7 @@ import exercise_C
 def main():
     """
     構築したモデルから重要度を取得し、重要度の高い順に特徴量を並べる
+    exercise_I.pyのmain関数を実行してから実行すること
     """
     with open("output/glf.pkl", "rb") as f:
         glf = pickle.load(f)
